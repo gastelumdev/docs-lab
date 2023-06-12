a7e11ba5bea5c727f9f75b4f73eec1eafb5696ce
Omar Gastelum <gastelumdev@gmail.com>
2023-06-11 23:03:58 -0700

    Fix api url

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,9 @@ require("dotenv").config({ path: path.resolve(__dirname, '../.env') });
 
 const connectDB = async () => {
     try {
-        // const conn = await mongoose.connect(process.env.MONGODB_URI);
         const conn = await mongoose.connect(process.env.DB_URL);
+        // const conn = await mongoose.connect("mongodb+srv://gastelumdev:2JHUX4jhJ02Pk5oP@collabtime-mern.tpmchkg.mongodb.net/?retryWrites=true&w=majority");
+        
         console.log(`Mongo db connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
--- client/src/features/api/api_url.ts
+++ client/src/features/api/api_url.ts
@@ -1,4 +1,11 @@
-const API_URL = process.env.REACT_API_URL || "http://localhost:4000"
+console.log(process.env.REACT_APP_DEBUG)
+
+const DEBUG = process.env.REACT_APP_DEBUG || "false";
+
+const API_URL = (process.env.REACT_APP_DEBUG == "true")
+    ? "http://localhost:4000"
+    : "https://docs-lab-backend.onrender.com";
+
 console.log(API_URL)
 
 export default API_URL;
